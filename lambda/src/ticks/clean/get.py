3ae579c392b102569559c668b27b28bc96cc2a18
Dan <dan@dgriffiths.io>
2022-07-08 16:22:28 +0100

    More live pipeline

--- lambda/src/ticks/clean/get.py
+++ lambda/src/ticks/clean/get.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 import pandas as pd
 import pytz
+from typing import Union
 
-# from IPython.display import display
+from IPython.display import display
 
 columns = [
     "symbol",
@@ -21,13 +22,20 @@ columns = [
 ]
 
 def get_clean_ticks(
-    data: dict
+    data: Union[dict, list]
 ):
-    df_ticks = pd.DataFrame(data["ticks"])
+    if isinstance(data, list):
+        df_ticks = pd.DataFrame(data)
+        df_ticks["timestamp_utc"] = df_ticks["time"]
+        df_ticks["timestamp_broker"] = df_ticks["brokerTime"]
+        df_ticks["timestamp_uk"] = df_ticks["timestamp_utc"].dt.tz_convert("Europe/London").dt.tz_localize(None)
+        df_ticks["timestamp_utc"] = df_ticks["timestamp_utc"].dt.tz_localize(None)
+    else:
+        df_ticks = pd.DataFrame(data["ticks"])
+        df_ticks["timestamp_utc"] = pd.to_datetime(df_ticks["time"].str[:17], format="%Y%m%d%H%M%S%f")
+        df_ticks["timestamp_broker"] = pd.to_datetime(df_ticks["brokerTime"], format="%Y-%m-%d %H:%M:%S.%f")
+        df_ticks["timestamp_uk"] = df_ticks["timestamp_utc"].dt.tz_localize("UTC").dt.tz_convert("Europe/London").dt.tz_localize(None)
     df_ticks[["ask", "bid"]] = df_ticks[["ask", "bid"]].replace(0.0, pd.NA)
-    df_ticks["timestamp_utc"] = pd.to_datetime(df_ticks["time"].str[:17], format="%Y%m%d%H%M%S%f")
-    df_ticks["timestamp_broker"] = pd.to_datetime(df_ticks["brokerTime"], format="%Y-%m-%d %H:%M:%S.%f")
-    df_ticks["timestamp_uk"] = df_ticks["timestamp_utc"].dt.tz_localize("UTC").dt.tz_convert("Europe/London").dt.tz_localize(None)
     df_ticks["hour"] = df_ticks["timestamp_utc"].dt.strftime("%H")
     df_ticks["date"] = df_ticks["timestamp_utc"].dt.strftime("%Y%m%d")
     df_ticks["month"] = df_ticks["date"].str[4:6]
@@ -38,5 +46,3 @@ def get_clean_ticks(
         if col not in df_ticks.columns:
             df_ticks[col] = pd.NA
     return df_ticks[columns]
-        
-    
